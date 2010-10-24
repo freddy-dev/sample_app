bf31bed589ce76f597013c69e78a580dd8629571
freddy_dev <friedrichin@gmail.com>
2010-10-24 21:38:05 +0530

    working on modeling users

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+# Schema version: 20100907111045
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+	attr_accessible :name, :email
+	
+	#validates_presence_of :name
+end
